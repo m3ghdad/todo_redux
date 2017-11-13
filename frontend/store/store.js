60cf0efd5720fef24dbd6bc1237737f8d8d3a937
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-13 14:16:39 -0800

    successfully test to see if window.store.getState() returns initial application state;

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,10 @@
 import {createStore} from 'redux';
-import rootReducer from '../reducers/root_reducer.js';
+import rootReducer from '../reducers/root_reducer';
+
+// const configureStore = (preloadedState = {}) => {
+//   const store = createStore(rootReducer, preloadedState);
+//   return store;
+// }
 
 const configureStore = createStore(rootReducer);
 
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-//
-// window.sotre = configureStore;
+
+const store = configureStore;
+//Remove after testing
+// -------------------
+window.sotre = store;
+
+
+//--------------------
+
 
 document.addEventListener('DOMContentLoaded', ()=> {
+
   ReactDOM.render(
     <h1>Todos App</h1>,
     document.getElementById('root')
