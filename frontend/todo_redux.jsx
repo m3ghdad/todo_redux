7a0fe072c84e51c8cc27d4be4a5421d45b552c3a
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-13 10:34:08 -0800

    Fixes issue where content wasnt loading on the browser

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 document.addEventListener('DOMContentLoaded', ()=> {
   ReactDOM.render(
+    <h1>Todos App</h1>,
     document.getElementById('root')
-  )
-})
+  );
+});
