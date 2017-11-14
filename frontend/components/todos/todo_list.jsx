e096a2bc14bce1c14d9df45901e59d3d3269162d
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-13 17:23:46 -0800

    Checkpoit for the day

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import todo_list_item from './todo_list_item';
 
 class TodoList extends React.Component {
   render() {
-    
+
     return (
-      <h3>TODO LIST</h3>
+      <div className="todo-list">
+        <h3>TODO LIST</h3>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
     )
-
   }
-
 }
 
 export default TodoList;
---
+++ frontend/components/todos/todo_list_item.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// 
+// const todo_list_item = ({item}) => (
+//   <div className="list-item">
+//     <h3>
+//       {item.title}
+//     </h3>
+//     <span>
+//       {item.body}
+//     </span>
+//     <span>
+//       {item.isDone}
+//     </span>
+//   </div>
+// )
+//
+// export default todo_list_item;
